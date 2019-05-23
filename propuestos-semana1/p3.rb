afdfcde8a230fa833ca329a041d00fc5f87d3190
Marlon Colca <marlon.colca.io@gmail.com>
2019-05-22 22:02:55 -0500

    la p3 y p4 de los propuestos de la semana 1 han sido convertidos a clases

--- propuestos-semana1/p3.rb
+++ propuestos-semana1/p3.rb
@@ -1,54 +1,65 @@
-def mesMayorVenta(meses, ventas)
-  venta_mayor = ventas[0]
-  mes_mayor = meses[0]
+class Program
 
-  for i in 0..ventas.size - 1 
-    if (ventas[i] > venta_mayor)
-      venta_mayor = ventas[i]
-      mes_mayor = meses[i]
+  def initialize ()
+    @nombre_meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"]
+    @ventas_primeros_meses = [10, 30, 80, 20, 40, 50]
+  end
+
+  def mesMayorVenta(meses, ventas)
+    venta_mayor = ventas[0]
+    mes_mayor = meses[0]
+
+    for i in 0..ventas.size - 1 
+      if (ventas[i] > venta_mayor)
+        venta_mayor = ventas[i]
+        mes_mayor = meses[i]
+      end
     end
+
+    puts "La mayor venta la tiene #{mes_mayor} con #{venta_mayor}"
   end
 
-  puts "La mayor venta la tiene #{mes_mayor} con #{venta_mayor}"
-end
+  def mesMenorVenta(meses, ventas)
+    venta_menor = ventas[0]
+    mes_menor = meses[0]
 
-def mesMenorVenta(meses, ventas)
-  venta_menor = ventas[0]
-  mes_menor = meses[0]
-
-  for i in 0..ventas.size - 1 
-    if (ventas[i] < venta_menor)
-      venta_menor = ventas[i]
-      mes_menor = meses[i]
+    for i in 0..ventas.size - 1 
+      if (ventas[i] < venta_menor)
+        venta_menor = ventas[i]
+        mes_menor = meses[i]
+      end
     end
+
+    puts "La menor venta la tiene #{mes_menor} con #{venta_menor}"
   end
 
-  puts "La menor venta la tiene #{mes_menor} con #{venta_menor}"
-end
-
-def promedioVentas(ventas)
-  suma = 0
-  for i in 0..ventas.size - 1
-    suma += ventas[i]
+  def promedioVentas(ventas)
+    suma = 0
+    for i in 0..ventas.size - 1
+      suma += ventas[i]
+    end
+    return suma/ventas.size
   end
-  return suma/ventas.size
-end
 
-def mesesVendenMayorAlPromedio(meses, ventas)
-  promedio = promedioVentas(ventas)
-  puts "Calculando meses con ventas mayores al promedio: "
+  def mesesVendenMayorAlPromedio(meses, ventas)
+    promedio = promedioVentas(ventas)
+    puts "Calculando meses con ventas mayores al promedio: "
   
-  for i in 0..ventas.size - 1
-    if (ventas[i] > promedio)
-      puts "\t #{meses[i]} con #{ventas[i]}"
+    for i in 0..ventas.size - 1
+      if (ventas[i] > promedio)
+        puts "\t #{meses[i]} con #{ventas[i]}"
+      end
     end
+  end
+
+  def reporte()
+    mesMayorVenta(@nombre_meses, @ventas_primeros_meses)
+    mesMenorVenta(@nombre_meses, @ventas_primeros_meses)
+    puts "El promedio de ventas de todos los meses es #{promedioVentas(@ventas_primeros_meses)}"
+    mesesVendenMayorAlPromedio(@nombre_meses, @ventas_primeros_meses)
   end
 end
 
-nombre_meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"]
-ventas_primeros_meses = [10, 30, 80, 20, 40, 50]
+program = Program.new()
+program.reporte()
 
-mesMayorVenta(nombre_meses, ventas_primeros_meses)
-mesMenorVenta(nombre_meses, ventas_primeros_meses)
-puts "El promedio de ventas de todos los meses es #{promedioVentas(ventas_primeros_meses)}"
-mesesVendenMayorAlPromedio(nombre_meses, ventas_primeros_meses)
--- propuestos-semana1/p4.rb
+++ propuestos-semana1/p4.rb
@@ -1,35 +1,39 @@
-def calculaBono (dni: '', nombre: '', apellido: '', sueldo_base: 0, tipo_trabajador: '')
-  bono = 0
-  case tipo_trabajador
-    when 'Planilla'
-      bono = 300
-    when 'Por Horas'
-      bono = 400
-    when 'Parcial'
-      bono = 500
-    else
-      puts 'TIPO DE TRABAJADOR NO VALIDO'
-  end
+class Program  
+  def calculaBono (dni: '', nombre: '', apellido: '', sueldo_base: 0, tipo_trabajador: '')
+    bono = 0
+    case tipo_trabajador
+      when 'Planilla'
+        bono = 300
+      when 'Por Horas'
+        bono = 400
+      when 'Parcial'
+        bono = 500
+      else
+        puts "#{dni} - TIPO DE TRABAJADOR NO VALIDO"
+    end
 
-  puts "El trabajador #{nombre} #{apellido} recibe un bono de #{bono} soles. Sueldo: #{sueldo_base + bono} soles"
+    puts "El trabajador #{nombre} #{apellido} recibe un bono de #{bono} soles. Sueldo: #{sueldo_base + bono} soles"
+  end
 end
 
-calculaBono(
+program = Program.new()
+
+program.calculaBono(
   dni: '12345678', nombre: 'Pepito', apellido: 'apel1',
   sueldo_base: 200, tipo_trabajador: 'Planilla'
 )
 
-calculaBono(
+program.calculaBono(
   dni: '12345678', nombre: 'Luchito', apellido: 'apel2',
   sueldo_base: 200, tipo_trabajador: 'Por Horas'
 )
 
-calculaBono(
+program.calculaBono(
   dni: '12345678', nombre: 'Pepita', apellido: 'apel3',
   sueldo_base: 200, tipo_trabajador: 'Parcial'
 )
 
-calculaBono(
-  dni: '12345678', nombre: 'Luchita', apellido: 'apel4',
+program.calculaBono(
+  dni: '66666666', nombre: 'Luchita', apellido: 'apel4',
   sueldo_base: 200, tipo_trabajador: 'Otro tipo'
 )
